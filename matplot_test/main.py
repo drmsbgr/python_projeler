4dad5406c91284c8ee5faebda12b15cae3dafdd7
drmsbgr <drmsbgr@gmail.com>
2024-03-29 11:13:31 +0300

    init

--- matplot_test/main.py
+++ matplot_test/main.py
@@ -1,11 +1,12 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-t = np.linspace(0, 2 * np.pi, 1024)
-data2d = np.sin(t)[:, np.newaxis] ** 2 + np.cos(t)[np.newaxis, :] ** 3
+
+t = np.linspace(-4, 4 * np.pi, 1024)
+data2d = np.exp(-np.cos(t)**2)[:, np.newaxis] * np.exp(-np.sin(t)**2)[np.newaxis, :]
 
 fig, ax = plt.subplots()
-im = ax.imshow(data2d)
+im = ax.imshow(data2d, cmap="jet")
 ax.set_title("deneme")
 
 fig.colorbar(im, ax=ax, label="Interactive colorbar")
